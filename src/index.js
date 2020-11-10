242125d8318b6fb0bd2d3ea4863dc6e44cd09f71
serhijko <sp_by@tut.by>
2020-11-10 05:27:23 -0300

    Config ESLint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 var $ = require('jquery');
 var str = require('./constant');
 
-var App = function () {
-    console.log(str);
+var App = function() {
+    // console.log(str);
     $('body').html(str);
 };
 
 var app = new App();
+app.get = function() {};
