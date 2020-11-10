d71f5f9408c04a17805c4346c58d8b624ae5e472
serhijko <sp_by@tut.by>
2020-11-10 02:33:31 -0300

    Create package.json Config Webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var webpack = require('webpack');
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    }
+
+};
