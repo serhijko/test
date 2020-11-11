2466b2fea913f28bf2be491b986e13c4bd4d14d8
serhijko <sp_by@tut.by>
2020-11-11 04:34:32 -0300

    Creating the first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>App works!</h1>
+                <h3>It's really working!</h3>
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
