1ef91b941bdcb7c6bf538023e8e613d8ab75a242
serhijko <sp_by@tut.by>
2020-11-11 03:33:13 -0300

    Creating React structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-## Test project
-Test project for study
+### React theory
+1. Run `npm i`
+2. Run `npm i eslint webpack-dev-server webpack eslint-plugin-react -g`
+3. Run `npm run dev`
+4. Open `localhost:3000`
--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-var $ = require('jquery');
-require('./less');
-
-var App = function() {
-    $('body').html('<div class="b-test"><h3 class="b-test__heading">Heading <div> </div></h3></div>');
-};
-
-var app = new App();
-app.get = function() {};
+console.log('Hi');
